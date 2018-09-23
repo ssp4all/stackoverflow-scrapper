67e1fd7994331c65954f4c0e75b26ad07acfb756
ssp <2015suraj.pawar@ves.ac.in>
2018-09-23 15:30:42 +0530

    Init:  Scraping

--- test/scrape.py
+++ test/scrape.py
@@ -4,19 +4,57 @@ import sys
 import os
 from bs4 import BeautifulSoup
 import requests
-from queue import Queue
 from subprocess import PIPE, Popen
-import webbrowser
 import time
 
-SO_URL = "https://stackoverflow.com"
 
-# ASCII color codes
-GREEN = '\033[92m'
-GRAY = '\033[90m'
-CYAN = '\033[36m'
-RED = '\033[31m'
-YELLOW = '\033[33m'
-END = '\033[0m'
-UNDERLINE = '\033[4m'
-BOLD = '\033[1m'
+########################################
+# Scrape this URL
+########################################
+SO_URL = "https://stackoverflow.com/search?q=array+comprehension"
+
+
+########################################
+# Logic to scrape
+########################################
+def get_search_results(soup):
+    """Returns a list of dictionaries containing each search result."""
+    search_results = []
+    posts = soup.find_all(class_="question-summary search-result")
+    for result in posts:
+        title = result.find(class_="result-link").get_text().replace('\n', '')
+        title_link = result.find(class_="result-link").find('a')['href']
+
+        if (result.find(class_="status answered")) != None:  # Has answers
+            answer_count = int(result.find(
+                class_="status answered").find("strong").get_text())
+        # Has an accepted answer (closed)
+        elif result.find(class_="status answered-accepted") != None:
+            answer_count = int(result.find(
+                class_="status answered-accepted").find("strong").get_text())
+        else:  # No answers
+            answer_count = 0
+
+        # answer_count = 0
+        search_results.append({
+            "Title": title,
+            #"Body": result.find_all("div", class_="excerpt")[0].text,
+            #"Votes": int(result.find_all("span", class_="vote-count-post ")[0].find_all("strong")[0].text),
+            "Answers": answer_count,
+            "URL": SO_URL + title_link
+        })
+
+    print(search_results)
+
+########################################
+# Main
+########################################
+
+def main():
+    response = requests.get(SO_URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    get_search_results(soup)
+
+
+if __name__ == "__main__":
+    main()
