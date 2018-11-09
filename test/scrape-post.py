d5d5169242981e86d964b7e419faf84c24838e96
ssp <2015suraj.pawar@ves.ac.in>
2018-11-09 19:21:11 +0530

    post scraping

--- test/scrape-post.py
+++ test/scrape-post.py
@@ -1,8 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-
-new_url = 'https://stackoverflow.com/questions/18166581/python-looping-through-a-multidimensional-dictionary'
+new_url = 'https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1733489#1733489'
 new_response = requests.get(new_url)
 new_soup = BeautifulSoup(new_response.text, 'html.parser')
 
@@ -10,20 +9,34 @@ new_title = new_soup.find(
     'h1', {'class': 'grid--cell fs-headline1 fl1'}).get_text().replace('\n', '')
 print()
 
-print('#'*100)
+print('#'*50)
 print(new_title)
-print('#'*100)
+print('#'*50, end="\n\n")
 
-post_bodies = new_soup.find_all(class_ = 'post-text')
-# print(post_bodies)
-for post_body in post_bodies:
-    # print(post_body)
-    pbody = post_body.find('p').get_text()
-    pcode = post_body.find('code').get_text()
-    print('-'*40)
-    print()
-    print(pbody)
-    print(pcode)
-    print()
-print()
+# post_bodies = new_soup.find_all(class_ = 'post-text')
+post_body = new_soup.find(class_ ='answer')
+
 # print(post_body)
+pbody = post_body.find_all('p')  #find all p
+for p in pbody:
+    print(p.get_text())
+    print()
+# print(pbody)
+
+pcode = post_body.find_all('code')  #find all code
+for c in pcode:
+    print(c.get_text())
+    print()
+# print(pcode)
+
+pvote = post_body.find(class_="vote").find('span').get_text()
+print()
+print('*'*18)
+print("*    VOTES : ",end = '')
+print(pvote+"   *")
+
+print('*'*18)
+
+
+print()
+print()
