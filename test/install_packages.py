3ad5d58c6afd8d41deb73bc41354996c85ccdca3
ssp <2015suraj.pawar@ves.ac.in>
2018-11-09 23:52:12 +0530

    update

--- test/install_packages.py
+++ test/install_packages.py
@@ -3,26 +3,7 @@
 import os
 import subprocess
 import sys
-
-
-def toString(byte):
-    return byte.decode("utf-8").strip()
-
-
-def run(args):
-    process = subprocess.Popen(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return process.communicate()
-
-
-def runrealtime(args):
-    process = subprocess.Popen(
-        args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    while process.poll() is None:
-        output = process.stdout.readline().decode("utf-8").strip()
-        print(output)
-    return process.communicate()
-
+from funtions import toString, run, runrealtime
 
 #
 # checking for python version 3
