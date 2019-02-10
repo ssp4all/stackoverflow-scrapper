d01c73406f33c50915a6ac78a6f0f9b76b0332f5
ssp <2015suraj.pawar@ves.ac.in>
2019-02-10 19:31:21 +0530

    Add

--- test/testing.py
+++ test/testing.py
@@ -4,8 +4,23 @@
 # print('hello'.capitalize(),end='')
 
 
-import webbrowser
-print('Enter b to open a link')
-if input() in ['b', 'B']:
-    url = 'fb.com'
-    webbrowser.open(url)
+# import webbrowser
+# print('Enter b to open a link')
+# if input() in ['b', 'B']:
+#     url = 'fb.com'
+#     webbrowser.open(url)
+
+# import re
+# str = "purple alice@google.com, blah monkey bob@abc.com blah dishwasher"
+
+# ## Here re.findall() returns a list of all the found email strings
+# # ['alice@google.com', 'bob@abc.com']
+# emails = re.findall(r'[w.-]+@[w.-]+', str)
+# print(emails)
+# print('ecit')
+# for email in emails:
+#     # do something with each found email string
+#     print(email)
+from pathlib import Path
+p = Path('test') / 'req.txt'
+print(p)
