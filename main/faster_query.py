8d891b5f90098ab08b516dd762cf5f1a51c5f73a
ssp <2015suraj.pawar@ves.ac.in>
2019-03-21 19:11:34 +0530

    changes done

--- main/faster_query.py
+++ main/faster_query.py
@@ -6,7 +6,10 @@ def faster_query(query):
     """Print code part for faster response."""
     
     output, error = run(["howdoi", query])
-    print_animal()
-    clear_terminal()
-    print(colored('OUTPUT:', 'white', attrs=['reverse', 'bold']))
-    print(colored(toString(output), 'yellow'))
+    if output is None:
+        print(colored('No result found', 'red', attrs='reverse'))
+    else:
+        print_animal()
+        clear_terminal()
+        print(colored('OUTPUT:', 'white', attrs=['reverse', 'bold']))
+        print(colored(toString(output), 'yellow'))
